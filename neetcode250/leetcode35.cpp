67bcb4f204d99144dc5a8b74e734d709653727e5
silviolet <charms@live.ca>
2025-09-06 11:38:18 -0400

    Create leetcode35.cpp

---
+++ neetcode250/leetcode35.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l <= r){
+            int mid = l + ((r - l) / 2);
+            int cur = nums[mid]; 
+            if (cur == target) return mid;
+            if (cur < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return l;
+    }
+};
