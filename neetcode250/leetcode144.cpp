d85f1bd6828b07a24e25b7e46ac5abdc84b476a0
silviolet <charms@live.ca>
2025-09-11 15:11:27 -0400

    Create leetcode144.cpp

---
+++ neetcode250/leetcode144.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        dfs(res, root);
+        return res; 
+    }
+    void dfs(vector<int>& res, TreeNode* curr){
+        if (curr == nullptr) return;
+        res.push_back(curr->val);
+        dfs(res, curr->left);
+        dfs(res, curr->right);
+    }
+};
