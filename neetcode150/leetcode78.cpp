dadc1c48317d2646e63a30d665a1bf72be13b18c
silviolet <charms@live.ca>
2025-09-18 03:42:00 -0400

    Fixed leetcode78.cpp

--- neetcode150/leetcode78.cpp
+++ neetcode150/leetcode78.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+    vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> cur;
-        dfs(res, nums, cur, 0, target, 0);
-        return res;
+        vector<int> curr;
+        dfs(nums, res, curr, 0);
+        return res; 
     }
-    void dfs(vector<vector<int>>& res, vector<int>& nums, vector<int>& cur, int index, int target, int total){
-        if (total > target || index >= nums.size()) return;
-        if (target == total){
-            res.push_back(cur);
+    void dfs(vector<int>& nums, vector<vector<int>>& subsets, vector<int>& curr, int index){
+        if (index == nums.size()){
+            subsets.push_back(curr);
             return;
         }
-        cur.push_back(nums[index]);
-        dfs(res, nums, cur, index, target, total + nums[index]);
-        cur.pop_back();
-        dfs(res, nums, cur, index + 1, target, total);
-        return;
+        curr.push_back(nums[index]);
+        dfs(nums, subsets, curr, index + 1);
+        curr.pop_back();
+        dfs(nums,subsets, curr, index + 1);
     }
 };
