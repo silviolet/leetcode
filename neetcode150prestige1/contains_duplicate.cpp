360bbab1f312a9db544166121f99a7bff1c976cc
silviolet <charms@live.ca>
2025-11-03 20:15:09 -0500

    Create contains_duplicate.cpp

---
+++ neetcode150prestige1/contains_duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+        for (const int& num : nums) {
+            if (seen.find(num) != seen.end()) return true;
+            seen.insert(num);
+        }
+        return false;
+    }
+};
