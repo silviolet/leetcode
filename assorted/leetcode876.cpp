4d1e74047c1b683016674e7f0de06b5bfaf76c07
silviolet <charms@live.ca>
2026-03-01 10:04:32 -0500

    Create leetcode876.cpp

---
+++ assorted/leetcode876.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+       ListNode* slow{head}, *fast{head->next};
+       while (fast) {
+        slow = slow->next;
+        if (!fast->next || !fast->next->next) return slow;
+        fast = fast->next->next;
+       }
+       return slow;
+    }
+};
